cfee268c1a59081a3c33d34c85577a5a20f69222
Sasawat Yimleang <framilly.eng@gmail.com>
2026-01-09 20:16:45 -0500

    Update index.html location

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 echo "===> Verifying build output..."
 
-test -f dist/html/index.html || (echo "Error: index.html not found!" && exit 1)
+test -f dist/index.html || (echo "Error: index.html not found!" && exit 1)
 
 echo "Verification passed!"
